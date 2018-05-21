1baca71fb6bfbc731458ec09e8f06f4c283a48c9
Kraaft <32433499+Kraaft@users.noreply.github.com>
2018-05-22 00:46:47 +0200

    Update puzzlextract.py

--- puzzlextract.py
+++ puzzlextract.py
@@ -24,27 +24,28 @@ count = 1
 gameID = ""
 manual_puzzles = {}
 
-# Ask if --manual option was entered
+# Ability to input multiple json files and define the --manual option
 parser = argparse.ArgumentParser()
 parser.add_argument("files", type=str, nargs="+")
 parser.add_argument("--manual", help="Add manual puzzles", action="store_true", default=False)
 args = parser.parse_args()
 
-# Launch the manual script if --manual
+# Launch the manual script if --manual was activated
 if args.manual:
 	from manual_input import dictio
 	manual_puzzles = dictio
 
+# Store the json files in a list
 json_puzzles = []
 for line in fileinput.input(args.files):
 	files_lines = line.strip()
-	# filter the blank lines
+	# Filter the blank lines
 	if re.match(r'^\s*$', files_lines):
 		pass
 	else:
 		json_puzzles.append(files_lines)
 
-# fetching pgn from lichess for data
+# Fetching pgn from lichess for data
 def fetch_lichess():
 	global w_player
 	global b_player
@@ -72,31 +73,32 @@ def fetch_lichess():
 	b_player_elo =  game.headers["BlackElo"]
 	timecontrol = game.headers["TimeControl"]
 
+	time.sleep(1)
+
 def extract():
 	global count
 	global gameID
 
-	# import json / parse last_pos & last_move & gameID
+	# Decrypt json & Record last_pos & last_move & gameID
 	json_tree = json.loads(line)
 	gameID = json_tree['game_id']
 	last_pos = json_tree['last_pos']
 	last_move = json_tree['last_move']
 
-	# final FEN position
+	# Create the final FEN position with last_pos+last_move
 	board = chess.Board(last_pos)
 	board.push_uci(last_move)
 	fen = board.fen()
 
-	# whitch turn to move
+	# Which turn to move
 	if board.turn == True:
 		turn = '&#9711; White'
 	else:
 		turn = '&#9899; Black'
 
 	fetch_lichess()
-	time.sleep(1)
 
-	# generate the html between each <td></td> using all the data
+	# Generate the html between each <td></td> using all the data (for auto puzzles)
 	td_str = """<td style="text-align: center;"><strong>{w} </strong>({we}) -<strong> {b} </strong>({be})<br /><a href="https://lichess.org/{id}">Gamelink</a>.<br />{t} to play.<br /><a href="https://lichess.org/analysis/{f}"><img src="http://www.fen-to-image.com/image/25/double/coords/{f}" /></a></td>"""
 	td = td_str.format(w=w_player, b=b_player, we=w_player_elo, be=b_player_elo, id=gameID, f=fen, t=turn)
 
@@ -141,26 +143,34 @@ if manual_puzzles:
 			turn = '&#9711; White'
 		else:
 			turn = '&#9899; Black'
+
 		# If gameID
 		try:
+			# Test if gameID is more than one character
 			temp_var_just_for_testing = ext_manual[0][1]
+
 			gameID = ext_manual[0]
 			extra_desc = ext_manual[1]
 			fetch_lichess()
-			time.sleep(1)
+
+			# Generate the html between each <td></td> using all the data (for manual puzzles with ID)
 			td_str = """<td style="text-align: center;"><strong>{w} </strong>({we}) -<strong> {b} </strong>({be})<br /><a href="https://lichess.org/{id}">Gamelink</a>.<br />{desc}<br />{t} to play.<br /><a href="https://lichess.org/analysis/{f}"><img src="http://www.fen-to-image.com/image/25/double/coords/{f}" /></a></td>"""
 			manual.append(td_str.format(w=w_player, b=b_player, we=w_player_elo, be=b_player_elo, id=gameID, f=fen, t=turn, desc=extra_desc))
 			print(Fore.CYAN + "Creation of puzzle n°" + Fore.YELLOW + "{0}".format(count) + Fore.RESET + " (" + Fore.MAGENTA + "manual" + Fore.RESET + ")")
 			count += 1
+
 		# If no gameID
 		except:
+			# Without ID there is still the description input
 			substitute = ext_manual[1]
+
+			# Generate the html between each <td></td> using all the data (for manual puzzles without ID)
 			td_str = """<td style="text-align: center;">{sub}<br />{t} to play.<br /><a href="https://lichess.org/analysis/{f}"><img src="http://www.fen-to-image.com/image/25/double/coords/{f}" /></a></td>"""
 			manual.append(td_str.format(f=fen, t=turn, sub=substitute))
 			print(Fore.CYAN + "Creation of puzzle n°" + Fore.YELLOW + "{0}".format(count) + Fore.RESET + " (" + Fore.MAGENTA + "manual" + Fore.RESET + ")")
 			count += 1
 
-# format to html
+# Format everything that is not data related to html
 header = """<p style="text-align: justify; margin-left: 40px;"><em>Click on the images for the solution.</em></p><table align="center" style="width:90%">"""
 tr_lichess4545 = """<tr><td colspan=4><p style="text-align:center"><a href="/team4545/"><img alt="team4545" src="https://image.ibb.co/hvqm3b/2018_01_21_2.png" /></a></p></td></tr>"""
 tr_lonewolf = """<tr><td colspan=4><p style="text-align:center"><a href="/lonewolf/"><img alt="Lonewolf" src="https://image.ibb.co/bETTGw/2018_01_21_3.png" /></a></p></td></tr>"""
@@ -171,23 +181,25 @@ tr_open = """<tr>"""
 tr_close = """</tr>"""
 foot = """</table><p>Chessboard images provided by&nbsp;<a href="http://lazydroid.com/">lenik terenin</a>.</p>"""
 
+# Starting writing HTML
 file_html.write(header)
 
-# repeat the script for each league separately
+# Repeat the script for each league separately
 def each_league(league, tr_league):
+	# Title (league) of the following puzzles
 	file_html.write(tr_league)
 
-	# caculate the number of row needed
+	# Caculate the number of row needed
 	row_calc = 0
 	for x in league:
 		row_calc += 1
 	row = row_calc // 4
 	rest = row_calc % 4
 
-	# increment variable for targeting the correct element in the list
+	# Increment variable for targeting the correct element in the list
 	u = 0
 
-	# generating complete rows = 4 puzzles
+	# Generating full rows (= 4 puzzles)
 	if row != 0:
 		for x in range(row):
 			file_html.write(tr_open)
@@ -197,7 +209,7 @@ def each_league(league, tr_league):
 			file_html.write(tr_close)
 			row -= 1
 
-	# last row or less than 4 puzzles in this league
+	# Last row or less than 4 puzzles in this league
 	if rest != 0:
 		file_html.write(tr_open)
 		for i in range(rest):
@@ -205,7 +217,7 @@ def each_league(league, tr_league):
 			u += 1
 		file_html.write(tr_close)
 
-# check if any entry before comiting the script
+# Check if any entry before comiting the script
 if lichess4545:
 	each_league(lichess4545, tr_lichess4545)
 if lonewolf:
@@ -217,10 +229,11 @@ if others:
 if manual:
 	each_league(manual, tr_manual)
 
+# Closing html
 file_html.write(foot)
 
+# End of the script
 file_html.close()
-
 print("\n\n# Output generated in the file \"generated_html_table.html\""
 "\n########################"
 "\n# " + Fore.YELLOW + "End of the Generator." + Fore.RESET +
